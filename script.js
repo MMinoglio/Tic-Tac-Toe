6f5a2917d58b481cd8d771cd231161021ac35ff0
MMinoglio <martin.minoglio@outlook.com>
2022-01-19 21:13:48 -0300

    diseñando html y css de pantalla de inicio

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ const player = (name, symbol) => {
 };
 
 const gameBoard = (() => {
-  const board = [];
+  const board = ["X",,"O",,"X",,"O",,"X"];
   const getState = () => board;
   const resetBoard = () => (board = []);
   const insertSymbol = (symbol, box) => (board[box] = symbol);
-  const checkWinner = (symbol) =>
+  const checkLineSymbol = (symbol) =>
     (board[0] === symbol && board[1] === symbol && board[2] === symbol) ||
     (board[3] === symbol && board[4] === symbol && board[5] === symbol) ||
     (board[6] === symbol && board[7] === symbol && board[8] === symbol) ||
@@ -19,13 +19,14 @@ const gameBoard = (() => {
     (board[0] === symbol && board[4] === symbol && board[8] === symbol) ||
     (board[2] === symbol && board[4] === symbol && board[6] === symbol);
 
-  return { getState, resetBoard, insertSymbol, checkWinner };
+  return { getState, resetBoard, insertSymbol, checkLineSymbol };
 })();
 
 const displayController = (() => {
   const listBoxes = () => document.querySelectorAll(".board-box");
   const boardState = () => gameBoard.getState();
   const selectBox = (box) => gameFlow.boxSelected(box);
+  const getStartWindow = () => document.querySelector(".modal-container");
   const refresh = () => {
     boardState().forEach((element, index) => {
       listBoxes()[index].textContent = element;
@@ -34,15 +35,22 @@ const displayController = (() => {
   listBoxes().forEach((box) => {
     box.addEventListener("click", (event) => {
       if (!!event.target.textContent) return;
-      selectBox(event.target.title);
+      selectBox(event.target.id);
     });
   });
+  const showStart = () => {
+    getStartWindow().classList.add("show");
+  }
+  refresh();
+  showStart();
+
   return { refresh };
 })();
 
 const gameFlow = (() => {
   let player1Turn = Math.random() > 0.5;
-  const isWinner = (player) => gameBoard.checkWinner(player.getSymbol());
+  
+  const isWinner = (player) => gameBoard.checkLineSymbol(player.getSymbol());
   const refresh = () => displayController.refresh();
   const insertSymbol = (symbol, box) => gameBoard.insertSymbol(symbol, box);
   const player1 = () => player("playerOne", "X");
@@ -55,9 +63,8 @@ const gameFlow = (() => {
     player1Turn = !player1Turn;
     refresh();
     if (isWinner(player)) {
-      console.log(`${player.getName()} is the winner!!!`);
+      console.log(`${player.getName()} is the winner!!!`)
     }
   };
-
   return { boxSelected };
 })();
